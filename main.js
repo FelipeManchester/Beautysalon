2c1969954f67ccfc2bc35c20afee7e76001c791c
Felipe Manchester <f.p2809@gmail.com>
2022-05-09 22:20:37 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('#hedar nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {})
+}
