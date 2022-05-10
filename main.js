ca44b939a3732e80f58cc4c38fa8ac9d68d88495
Felipe Manchester <f.p2809@gmail.com>
2022-05-10 18:46:46 -0300

    Menu
    Acrescentado Javascript no Menu e terminando a seção "Sobre"

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-const nav = document.querySelector('#hedar nav')
+/*ABRE E FECHA O MENU QUANDO CLICAR NO ICONE */
+
+const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {})
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* QUANDO CLICAR EM UM ITEM DO MENU, ESCONDER O MENU */
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  link.addEventListener('click', function (){
+    nav.classList.remove('show')
+  })
 }
