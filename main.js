f2c04f4e25be0702f657b328db08838bf8f60ae2
Felipe Manchester <f.p2809@gmail.com>
2022-05-11 19:46:06 -0300

    1.3

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ for(const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* MUDAR O HEADER DA PÁGINA QUANDO DER SCROLL */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // Scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // Menor que a altura do header 
+    header.classList.remove('scroll')
+  }
+})
