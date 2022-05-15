42bdd44b2727b58e889c4d752e8edd2b7379d41f
Felipe Manchester <f.p2809@gmail.com>
2022-05-14 22:17:49 -0300

    Implementa Footer e botão para topo

--- main.js
+++ main.js
@@ -55,7 +55,20 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
  `,
   { interval: 100 }
 )
+
+
+// BOTÃO VOLTAR PARA O TOPO            
+
+const backToTopButton = document.querySelector('.back-to-top')
+ window.addEventListener('scroll', function () {
+  if (window.scrollY >= 1000) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
